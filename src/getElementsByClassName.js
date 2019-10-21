4130f3f4af1d26237900fbd4efeba7fd9c87a583
kwillard95 <kwillard95@gmail.com>
2019-10-21 16:29:38 -0700

    working on getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,39 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+//create array using document.body.children
+//iterate thru children
+  //base case: if no children then check class list
+  // if first element of class list matches then push that element into result
+
+// if there are children then recurse the getlements by class name on the children
+var getElementsByClassName = function(className) {
   // your code here
+
+
+  var result = [];
+  var childrenArr = document.body.children;
+
+  //define inner function that takes in part of element tree
+  var innerFunc = function(branch) {
+    for (var i = 0; i < branch.length; i++ ) {
+      if (branch[i].children.length === 0 ) {
+        var list = branch[i].children.classList;
+        if (list[0] === className) {
+          result.push(branch[i].children);
+        }
+      } else {
+        var innerChild = innerFunc(branch[i].children);
+        result.concat(innerChild);
+      }
+
+    }
+    return result;
+  };
+  //invoke that inner function, and that is recursed
+  innerFunc(childrenArr);
+
+
 };
+
